ce35294bae79785dccd40055303fa92d102d4186
AlexNik13 <4pnav21@gmail.com>
2021-11-18 13:39:54 +0200

    add  Attention  from fish

--- src/main/java/aquariumTwo/animal/fish/Fish.java
+++ src/main/java/aquariumTwo/animal/fish/Fish.java
@@ -2,9 +2,15 @@ package aquariumTwo.animal.fish;
 
 import aquariumTwo.animal.Animal;
 import aquariumTwo.animal.behaviors.SpeedBehavior;
+import aquariumTwo.animal.behaviors.ambush.AmbushBehavior;
 
 public class Fish extends Animal {
+
     public Fish(int weight, SpeedBehavior behavior) {
         super(weight, behavior);
+    }
+
+    public Fish(int weight, SpeedBehavior speedBehavior, AmbushBehavior ambushBehavior) {
+        super(weight, speedBehavior, ambushBehavior);
     }
 }
--- src/main/java/aquariumTwo/animal/fish/eatable/Danio.java
+++ src/main/java/aquariumTwo/animal/fish/eatable/Danio.java
@@ -2,13 +2,14 @@ package aquariumTwo.animal.fish.eatable;
 
 import aquariumTwo.animal.Eatable;
 import aquariumTwo.animal.behaviors.BaseSpeedFish;
+import aquariumTwo.animal.behaviors.ambush.Attention;
 import aquariumTwo.animal.fish.Fish;
 
 import java.util.Formatter;
 
 public class Danio extends Fish implements Eatable {
     public Danio(int weight) {
-        super(weight, new BaseSpeedFish(0.4));
+        super(weight, new BaseSpeedFish(0.4), new Attention());
     }
 
     @Override
